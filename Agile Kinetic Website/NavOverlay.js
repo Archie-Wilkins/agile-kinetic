e71629f6ddd64c54c79b77ef0490ea6a42f0b18c
Archie Wilkins <wilkinsat1@cardiff.ac.uk>
2020-11-24 15:04:07 +0000

    Started work on desktop nav - Archie

--- Agile Kinetic Website/NavOverlay.js
+++ Agile Kinetic Website/NavOverlay.js
@@ -1,9 +1,7 @@
 function Navo(){
   document.getElementById('navBar').style.width="200px"
-  // document.getElementByID('navBar').style.width = '200px'
 }
 
 function Navc(){
   document.getElementById('navBar').style.width="0px"
-  // document.getElementByID("navBar").style.width="0"
 }
