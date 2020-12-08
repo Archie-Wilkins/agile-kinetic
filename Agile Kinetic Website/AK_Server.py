25f6a900490f60f9bc4fd117f23eea88e48ab71a
Archie Wilkins <wilkinsat1@cardiff.ac.uk>
2020-12-08 15:58:25 +0000

    Pushing up server connect code for admin page and faq form - Archie

--- Agile Kinetic Website/AK_Server.py
+++ Agile Kinetic Website/AK_Server.py
@@ -3,27 +3,11 @@ from flask import Flask, redirect, request, render_template, jsonify
 import sqlite3
 
 
-#Please Note Many of the later app routes which are commented out
-#and have placeholder written next to them
-#are (as you may have guessed) placeholders
-
-#To make them functional simply uncomment them and correct the
-#render template name
-#The placeholders are unlikely to have been properly tested if you
-#do uncomment them
-#There is also a piece of code below for inserting information into
-#the database, this is very much untested, but I thought it might be
-#useful later on :)
-
-#-Love from Archie
-#p.s any questions just let me know
-
 #=====================================================================
 # Need to insert data base name below
-#DATABASE = ''
-#ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+DATABASE = 'FAQ.db'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 #=====================================================================
-app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
@@ -55,28 +39,29 @@ def returnFourth():
         print("Admin Page Accessed")
         return render_template('adminBlock.html')
 
-#-----------WORK IN PROGRESS----------------------------------------
+#-----------WORK IN PROGRESS----------------------------------------------------
     #FAQ Page Post To
-#    if request.method == 'POST':
-#        faqQuestion = request.form.get("faqQuestion", default="Error")
-#	    faqAnswer = request.form.get("faqAnswer", default="Error")
-#		print("inserting module" + faqAnswer)
-#	    try:
-#		    print('Inserting')
-#			conn = sqlite3.connect(DATABASE)
-#			cur = conn.cursor()      #NEED TO CHANGE PLACEHOLDER TO NAME OF DATABASE TABLE
-#			cur.execute("INSERT INTO PLACEHOLDER ('faqQuestion','faqAnswer')\
-#			        VALUES (?,?)",(faqQuestion,faqAnswer) )
-#
-#			print("Still Inserting")
-#			conn.commit()
-#			msg =  faqQuestion + "has been added"
-#		except:
-#			conn.rollback()
-#			msg = "error in insert, please try again"
-#		finally:
-#			conn.close()
-#			return msg
+    if request.method == 'POST':
+        faqQuestion = request.form.get("faqQuestion", default="Error")
+	    faqAnswer = request.form.get("faqAnswer", default="Error")
+		print("inserting module" + faqAnswer)
+	    try:
+		    print('Inserting')
+			conn = sqlite3.connect(DATABASE)
+			cur = conn.cursor()      #NEED TO CHANGE PLACEHOLDER TO NAME OF DATABASE TABLE
+			cur.execute("INSERT INTO FAQ ('faqQuestion','faqAnswer')\
+			        VALUES (?,?)",(faqQuestion,faqAnswer) )
+
+			print("Still Inserting")
+			conn.commit()
+			msg =  faqQuestion + "has been added"
+		except:
+			conn.rollback()
+			msg = "error in insert, please try again"
+		finally:
+			conn.close()
+			return msg
+#-------------------------------------------------------------------------------
 
 
 #Route to Patients page - PLACE HOLDER
